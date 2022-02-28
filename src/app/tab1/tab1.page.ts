e7d4fe0d15e0634109be12df32f93577d16cda3c
Gildson Costacode <gildsoncosta@gmail.com>
2022-02-27 21:57:39 -0300

    adicionamos array de filmes e a interface de filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
+import { IFilme } from '../models/iFilme.model';
+import { Title } from '@angular/platform-browser';
 
 
 @Component({
@@ -10,7 +12,39 @@ import { ToastController } from '@ionic/angular';
 })
 export class Tab1Page {
 
-  constructor(public alertController: AlertController, public toastController: ToastController) { }
+  titulo = 'Vídeos App';
+
+
+  listaVideos: IFilme[] = [
+    {
+      nome: 'Kings Man: A Origem (2021)',
+      lancamento: '06/01/2022 (BR)',
+      duracao: '2h 10m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/pVL9AyKKLfUwrYD6jhdsI15gBQ7.jpg',
+      generos: ['Ação', 'Aventura', 'Thriller', 'Guerra']
+    },
+    {
+      nome: 'Ghostbusters: Mais Além (2021)',
+      lancamento: '8/11/2021',
+      duracao: '2h 4m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/719aIhGrTrJ0yzkZ806Ea4rbonV.jpg',
+      generos: ['Fantasia', 'Comédia', 'Aventura']
+    },
+    {
+      nome: 'Liga da Justiça',
+      lancamento: '16/12/2016',
+      duracao: '1h 26m',
+      classificacao: 10,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/mg9Nlc1GXw0ihf1Ha0BSTZ5fd8D.jpg',
+      generos: ['Action', 'Adventure']
+    }
+  ];
+
+  constructor(public alertController: AlertController, public toastController: ToastController, private titleService: Title) {
+    this.titleService.setTitle(this.titulo);
+  }
 
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
